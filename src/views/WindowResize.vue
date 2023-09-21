a16835292383d4644b7e7064dc9b2ed1ec64672c
prettywang2002 <shelly.tycg@gmail.com>
2023-09-21 23:56:55 +0800

    feat:加入斷點設計

--- src/views/WindowResize.vue
+++ src/views/WindowResize.vue
@@ -1,23 +1,37 @@
 <template>
-  <p>螢幕寬：{{ width }} x 螢幕高：{{ height }}</p>
+  <p class="text-xl">螢幕寬：{{ width }} x 螢幕高：{{ height }}</p>
 
-  <p>我是斷點：{{ breakPoint }}</p>
+  <p class="text-xl">我是斷點：{{ breakPoint }}</p>
+
+  <div class="container pt-8">
+    <button class="px-4 py-2 bg-cyan-500 text-white" v-if="breakPoint === 'default'">
+      我是575以下會出現
+    </button>
+    <button class="px-4 py-2 bg-cyan-500 text-white" v-if="breakPoint === 'sm'">
+      我是576以上會出現
+    </button>
+    <button
+      class="px-4 py-2 bg-cyan-500 text-white"
+      v-if="breakPoint === 'md' || breakPoint === 'lg'"
+    >
+      我是768以上才會出現
+    </button>
+  </div>
 </template>
 
 <script setup>
-import { watch, computed } from 'vue';
-import { useWindowSize } from '@vueuse/core';
+import { watch, computed } from "vue";
+import { useWindowSize } from "@vueuse/core";
 const { width, height } = useWindowSize();
 const breakPoint = computed(() => {
-  debugger;
   if (width.value >= 960) {
-    return 'lg';
+    return "lg";
   } else if (width.value >= 768) {
-    return 'md';
+    return "md";
   } else if (width.value >= 576) {
-    return 'sm';
+    return "sm";
   } else {
-    return 'default';
+    return "default";
   }
 });
 </script>
